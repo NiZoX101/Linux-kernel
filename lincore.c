f1695d81231182a87d2ba714aee021f7f0cf2b37
NiZoX <104158784+NiZoX101@users.noreply.github.com>
2023-12-18 23:13:49 +0700

    Initial commit

---
+++ lincore.c
@@ -0,0 +1,66 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/version.h>
+
+#define procfs_name "tsu"
+
+static struct proc_dir_entry* our_proc_file = NULL;
+
+static ssize_t procfile_read(struct file* file_pointer, char __user* buffer, size_t buffer_length, loff_t* offset)
+{
+	int len;
+	char s[32];  // Буфер для хранения строки с количеством ядер
+
+	len = snprintf(s, sizeof(s), "Number of CPUs: %d\n", num_online_cpus());
+
+	if (*offset > 0 || buffer_length < len)
+		return 0;  // Файл прочитан до конца или буфер слишком мал
+
+	if (copy_to_user(buffer, s, len))
+		return -EFAULT;
+
+	*offset += len;
+	pr_info("procfile read %s\n", file_pointer->f_path.dentry->d_name.name);
+
+	return len;
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+static const struct proc_ops proc_file_fops =
+{
+	.proc_read = procfile_read,
+};
+#else
+static const struct file_operations proc_file_fops =
+{
+	.read = procfile_read,
+};
+#endif
+
+static int __init procfs1_init(void)
+{
+	our_proc_file = proc_create(procfs_name, 0644, NULL, &proc_file_fops);
+
+	if (!our_proc_file)
+	{
+		pr_err("Error creating proc file\n");
+		return -ENOMEM;
+	}
+
+	pr_info("/proc/%s created\n", procfs_name);
+	return 0;
+}
+
+static void __exit procfs1_exit(void)
+{
+	proc_remove(our_proc_file);
+	pr_info("/proc/%s removed\n", procfs_name);
+}
+
+module_init(procfs1_init);
+module_exit(procfs1_exit);
+
+MODULE_LICENSE("GPL");
